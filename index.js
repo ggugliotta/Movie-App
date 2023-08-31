5a7d8667812e47833c74ba367411e96fa9dd788f
ggugliotta <gabriellagugliotta96@gmail.com>
2023-08-31 15:35:30 -0500

    node modules

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const app = express();
+
+let Movies = [
+  {
+    title: "Pride & Prejudice",
+    director: "Joe Wright",
+  },
+  {
+    title: "Where The Heart Is",
+    director: "John Boorman",
+  },
+  {
+    title: "The Big Short",
+    director: "Adam McKay",
+  },
+  {
+    title: "My Big Fat Greek Wedding",
+    director: "Joel Zwick",
+  },
+  {
+    title: "Mulan",
+    director: "Tony Bancroft, Barry Cook",
+  },
+  {
+    title: "Selena",
+    director: "Gregory Nava",
+  },
+  {
+    title: "The Phantom Menace",
+    director: "George Lucas",
+  },
+  {
+    title: "It/s a Wonderful Life",
+    director: "Frank Capra",
+  },
+  {
+    title: "Arsenic and Old Lace",
+    director: "Frank Capra",
+  },
+  {
+    title: "Steel Magnolias",
+    director: "Herbert Ross",
+  },
+];
+
+// GET requests
+app.get("/", (req, res) => {
+  res.send("Welcome to my movie API!");
+});
+
+app.get("/documentation", (req, res) => {
+  res.sendFile("public/documentation.html", { root: _dirname });
+});
+
+app.get("/movies", (req, res) => {
+  res.json(Movies);
+  no;
+});
+
+// USE requests
+
+app.use("/documentation", express.static("public"));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+}); //error middleware
+
+// listen for requests
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.");
+});
