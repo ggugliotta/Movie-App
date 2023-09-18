d02b4ca01505427dd17fa750019d7d33c345821e
Gabriella Gugliotta <gabriellagugliotta96@gmail.com>
2023-09-18 09:10:58 -0500

    added cors allowed domains

--- index.js
+++ index.js
@@ -19,8 +19,18 @@ mongoose.connect('mongodb://localhost:27017/gabriellaDB', {
 app.use(morgan("common"));
 app.use(express.static("public"));
 
-const cors = require("cors");
-app.use(cors());
+let allowedOrigins = ["http://localhost:8080"];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn't found on the list of allowed origins
+      let message = "The CORS policy for this application does not allow access from origin" + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require("./auth")(app);
 const passport = require("passport");
